cf3bd1e106e84a5d54d29bd2f2cce12400199fc8
peetonn <gpeetonn@gmail.com>
2019-08-02 23:27:39 -0700

    updated to 0.0.2

--- Formula/touchndn.rb
+++ Formula/touchndn.rb
@@ -3,8 +3,10 @@
 class Touchndn < Formula
   desc "TouchNDN helper code"
   homepage "https://touchndn.com"
-  url "https://github.com/remap/TouchNDN/archive/touchndn-helper-0.0.1.tar.gz"
-  sha256 "b3c87e1b0efa33a4efb43a0f00bd61bc5580d12151efbc02a9c2269bee1deeba"
+  url "https://github.com/remap/TouchNDN/archive/touchndn-helper-0.0.2.tar.gz"
+  sha256 "99864c0251426e01bc556fe3f78f7ea0d0ae759b9a540041cc8b940cf5f23714"
+
+  depends_on "spdlog"
 
   def install
     Dir.chdir("helper")
